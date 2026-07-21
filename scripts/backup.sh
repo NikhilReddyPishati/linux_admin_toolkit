70aca7e3464da28b169aab0eba1a6c5ecd929e0d
Nikhil Reddy Pishati <nikhilreddypishati8@gmail.com>
2026-07-21 18:59:43 +0530

    Refactor color variables and echo statements

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # colors
-GREEN="\e[32m"
-RED="\e[31m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-RESET="\e[0m"
+green="\e[32m"
+red="\e[31m"
+yellow="\e[33m"
+blue="\e[34m"
+reset="\e[0m"
 
 echo
 echo "******************************************"
 echo
-echo    -e "${BLUE}     💾BACKUP MANAGER${RESET}"
+echo    -e "${blue}     💾BACKUP MANAGER${reset}"
 echo
 echo "******************************************"
 read -p "Please enter your directory to Backup : " bck_file
 bck_file="../projects/$bck_file"
 if [ ! -d "$bck_file" ]
 then
-echo -e "${RED} sorry no directory found${RESET}"
+echo -e "${red} sorry no directory found${reset}"
 exit 1
 else
 mkdir -p ../backups
 backup_name="backup_$(date +%y%m%d_%H%M%S).tar.gz"
 tar -czf "../backups/$backup_name" "$bck_file"
 echo
-echo -e "${GREEN} Backup Created${RESET}"
-echo -e "${YELLOW} Backup file : $backup_name${RESET}"
+echo -e "${green} Backup Created${reset}"
+echo -e "${yellow} Backup file : $backup_name${reset}"
 echo "$(date '+%y%m%d_%H%M%S'): Backup created successfully,Backup FileName:$backup_name" >> ../logs/toolkit.log
 read -p "If you want to see content of the file press only (y/n)" choice
-if [ "$choice" == "y" ]
+if [ "$choice" = "y" ] |[ "$choice" = "Y" ] 
 then
 echo
 tar -tf "../backups/$backup_name"
