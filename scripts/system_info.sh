b8b1c20f50a275af89b2480f9772eb10bcf63f4d
Nikhil Reddy Pishati <nikhilreddypishati8@gmail.com>
2026-07-28 16:36:04 +0530

    Enhance system_info.sh with color-coded output
    Updated system_info.sh to include colored output for system information.

--- scripts/system_info.sh
+++ scripts/system_info.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
+green="\e[32m"
+red="\e[31m"
+yellow="\e[33m"
+blue="\e[34m"
+magentha="\e[35m"
+cyan="\e[36m"
+white="\E[37m"
+black="\E[30m"
+reset="\e[0m"
 
 echo "********************************************"
 echo 
-echo "           SYSTEM INFORMATION               "
+echo -e "${yellow}           SYSTEM INFORMATION       ${reset}        "
 echo 
 echo "********************************************"
 host_name=$(hostname)
 date=$(date)
-echo "Hostname               : $host_name"
-echo "Current user           : $USER"
-echo "Home Directory         : $HOME"
-echo "Current Directory      : $PWD"
-echo "Current Date           : $date"
-echo "kernel version         : $(uname -r)"
-echo "system Uptime          : $(uptime -p)"
-echo "IP Address             : $(hostname -I)"
+echo -e "${blue}Hostname               : $host_name ${reset}"
+echo -e "${magentha} Current user           : $USER ${reset}"
+echo -e "${cyan} Home Directory         : $HOME ${reset}"
+echo -e "${yellow} Current Directory      : $PWD ${reset}"
+echo -e "${green}Current Date           : $date ${reset}"
+echo -e "${red} kernel version         : $(uname -r) ${reset}"
+echo -e "${white} system Uptime          : $(uptime -p) ${reset}"
+echo -e "${green} IP Address             : $(hostname -I) {reset}"
 echo "*****************************************"
-echo "             MEMORY USAGE :               "
+echo -e "${green}             MEMORY USAGE ${reset}:               "
 echo "***************************************"
 free -h
 
 echo "***************************************"
-echo :"            DISK USAGE : "
+echo -e "${green}           DISK USAGE : ${reset}"
 echo "***************************************"
 df -h
