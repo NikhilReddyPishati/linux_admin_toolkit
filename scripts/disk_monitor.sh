6c4342ae7cae0dbab87a551cd764715bd93432d1
Nikhil Reddy Pishati <nikhilreddypishati8@gmail.com>
2026-07-28 16:25:51 +0530

    Add disk monitoring header to disk_monitor.sh

--- scripts/disk_monitor.sh
+++ scripts/disk_monitor.sh
@@ -9,6 +9,7 @@ cyan="\e[36m"
 white="\E[37m"
 black="\E[30m"
 reset="\e[0m"
+
 echo -e "${magentha} =============================================================================================================== ${reset}"
 echo
 echo -e "${cyan}                                            💾 DISK MONITORING     ${reset}"
